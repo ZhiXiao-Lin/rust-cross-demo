be3df2e90487c3a57c73f67e3cf20c8f51dddaba
roylin <1002591652@qq.com>
2022-03-18 04:41:35 +0000

    feat: hello

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate libloading as libc;
+use std::ffi::CString;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct GoString {
+    pub p: *const ::std::os::raw::c_char,
+    pub n: isize,
+}
+
+fn call_go(go_name: GoString) -> Result<u32, Box<dyn std::error::Error>> {
+    unsafe {
+        #[cfg(target_os = "linux")]
+        let libc = libc::Library::new("./libs/linux/libhello.so")?;
+        #[cfg(target_os = "macos")]
+        let libc = libc::Library::new("./libs/macos/libhello.so")?;
+        let func: libc::Symbol<unsafe extern "C" fn(GoString)> = libc.get(b"Hello")?;
+        func(go_name);
+        Ok(0)
+    }
+}
+
+fn main() {
+    let c_name = CString::new("Alex").unwrap();
+
+    let go_str_ref = GoString {
+        p: c_name.as_ptr(),
+        n: c_name.as_bytes().len() as isize,
+    };
+
+    let _ = call_go(go_str_ref);
+}
